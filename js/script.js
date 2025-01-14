1363bcb838d2d03ae35808171cbe61ce4cc6187e
Gia Miminoshvili <miminoshvili2102@gmail.com>
2025-01-14 15:57:25 +0400

    update v1.5

--- js/script.js
+++ js/script.js
@@ -1,9 +1,22 @@
 const cursor = document.querySelector('.custom-cursor');
+const cursorOutline = document.querySelector('.custom-cursor-outline');
+
 let targetX = 0;
 let targetY = 0;
 let currentX = 0;
 let currentY = 0;
 const speed = 0.2;
+
+let targetXOutline = 0;
+let targetYOutline = 0;
+
+document.addEventListener('mousemove', (e) => {
+  targetX = e.clientX;
+  targetY = e.clientY;
+
+  targetXOutline = e.clientX;
+  targetYOutline = e.clientY;
+});
 
 function updateCursor() {
   currentX += (targetX - currentX) * speed;
@@ -18,12 +31,18 @@ function updateCursor() {
   requestAnimationFrame(updateCursor);
 }
 
-document.addEventListener('mousemove', (e) => {
-  targetX = e.clientX;
-  targetY = e.clientY;
-});
+function updateCursorOutline() {
+  const scrollX = window.scrollX;
+  const scrollY = window.scrollY;
+
+  cursorOutline.style.left = `${targetXOutline + scrollX}px`;
+  cursorOutline.style.top = `${targetYOutline + scrollY}px`;
+
+  requestAnimationFrame(updateCursorOutline);
+}
 
 updateCursor();
+updateCursorOutline();
 
 const header = document.querySelector('header');
 
@@ -36,3 +55,15 @@ document.addEventListener("scroll", () => {
     header.style.transition = "all .5s ease-in-out";
   }
 });
+
+document.addEventListener("scroll", ()=> {
+  console.log(window.scrollY);
+  
+})
+
+const scrollToY = (Y) => {
+  window.scrollTo({
+    top: Y,
+    behavior: 'smooth'
+  })
+}
